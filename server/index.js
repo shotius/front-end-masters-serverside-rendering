9b0f2fa41f0026177beb4ea75115b484cf5a3d76
shotius <sh.archemashvili@gmail.com>
2021-06-03 12:24:01 +0400

    streaming node server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { renderToString } from "react-dom/server";
+import { renderToNodeStream, renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 import fs from "fs";
 import App from "../src/App";
@@ -15,6 +15,7 @@ const app = express();
 app.use("/dist", express.static("dist"));
 
 app.use((req, res) => {
+  res.write(parts[0])
   const staticContext = {};
   const reactMarkup = (
     <StaticRouter url={req.url} context={staticContext}>
@@ -22,9 +23,15 @@ app.use((req, res) => {
     </StaticRouter>
   );
 
-  res.status(staticContext.statusCode || 200);
-  res.send(`${parts[0]}${renderToString(reactMarkup)}${parts[1]}`);
-  res.end();
+  const stream = renderToNodeStream(reactMarkup)
+
+  stream.pipe(res, { end: false })
+
+  stream.on('end', () => {
+    res.status(staticContext || 200);
+    res.write(parts[1]);
+    res.end();
+  })
 });
 
 console.log(`listening on http://localhost:${PORT}`);
